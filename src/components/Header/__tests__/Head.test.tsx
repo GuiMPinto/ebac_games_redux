52802fb5d1d49ad9ab71230c7621bdb80b942445
PintoJR <guimoraespinto@gmail.com>
2025-10-06 18:05:41 -0300

    V1 Error Mod_33 Faça testes com React e Redux

---
+++ src/components/Header/__tests__/Head.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen} from '@testing-library/react';
+
+import Header from "..";
+
+describe('Testes para o componente Header',() => {
+  test('Deve renderizar corretamente', () =>{
+    render(<Header />)
+    expect(screen.getByText('EBAC Games')).toBeInTheDocument()
+
+  })
+})
